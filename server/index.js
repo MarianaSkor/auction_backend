a781cab77ebcd8444cd1ad4443950003a0c665a1
Mariana <skorodynska2411@gmail.com>
2022-05-07 20:17:12 +0300

    set up

--- server/index.js
+++ server/index.js
@@ -1,15 +1,27 @@
-// server/index.js
-
 const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/user");
+const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
+const stripeRoute = require("./routes/stripe");
+const cors = require("cors");
 
 const PORT = process.env.PORT || 3001;
 
-const app = express();
+dotenv.config();
 
-app.get("/api", (req, res) => {
-    res.json({ message: "Hello from server!" });
-  });
-  
-  app.listen(PORT, () => {
-    console.log(`Server listening on ${PORT}`);
-  });
+mongoose
+  .connect(
+    "mongodb+srv://auction-admin:JiTTIonrDafr7HUe@cluster0.ulrvw.mongodb.net/auction?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("DB Connection Successfull"))
+  .catch((err) => console.log(err));
+
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
